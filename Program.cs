b23aa176d4d5e5e2bf5b44f2610af8edb39e17fc
ORIFJON NARKULOV <orifjon9@gmail.com>
2019-07-17 21:41:44 -0600

    Add ShellSort

--- Program.cs
+++ Program.cs
@@ -8,8 +8,14 @@ namespace Algorithms
         {
             //DC.Main.Run();
 
-            var sortedArray = Sort.Selection.Sort(new int[] {7, 10, 5, 3, 8, 4, 2, 9, 6});
-            Console.WriteLine(String.Join(',', sortedArray));
+            // var sortedArray = Sort.Selection.Sort(new int[] {7, 10, 5, 3, 8, 4, 2, 9, 6});
+            // Console.WriteLine(String.Join(',', sortedArray));
+
+            // var sortedArray = Sort.Insertion.Sort(new int[] {7, 10, 5, 3, 8, 4, 2, 9, 6});
+            // Console.WriteLine(String.Join(',', sortedArray));
+
+            var shellSort = Sort.ShellSort.Sort(new string[] { "S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E"});
+            Console.WriteLine(String.Join(',', shellSort));
         }
     }
 }
