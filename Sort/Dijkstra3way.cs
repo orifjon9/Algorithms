4604c3e7b0f99f179f4290f2e1cb55d2bbe3de7e
Orifjon Narkulov <orifjon9@gmail.com>
2019-07-24 22:27:45 -0600

    add Dijkstra3way initial rules (#15)

---
+++ Sort/Dijkstra3way.cs
@@ -0,0 +1,10 @@
+namespace Sort
+{
+    public class Dijkstra3way
+    {
+        // v = arr[lo]
+        // arr[i] < v then swap arr[lt] with arr[i] then lt++ and i++
+        // arr[i] > v then swap arr[gt] with arr[i] then gt-- 
+        // if equals then just i++;
+    }
+}
