5fbdaea8932f07d828e9114de0f5a53088a5256f
ORIFJON NARKULOV <orifjon9@gmail.com>
2019-08-04 22:55:44 -0600

    Add BST Put method

--- DS/RedBlackTree.cs
+++ DS/RedBlackTree.cs
@@ -5,15 +5,121 @@ namespace DS
     public class RedBlackTree<TKey, TValue>
         where TKey : IComparable
     {
+        private Node _root;
 
         public void Put(TKey key, TValue value)
         {
-
+            _root = Put(_root, key, value);
         }
 
-        public TValue GetValue(TKey key)
+        private Node Put(Node node, TKey key, TValue value)
         {
-            return default;
+            if (node == null)
+            {
+                node = new Node(key, value);
+            }
+            else
+            {
+                var comp = key.CompareTo(node.Key);
+                if (comp < 0)
+                {
+                    node.Left = Put(node.Left, key, value);
+                }
+                else if (comp > 0)
+                {
+                    node.Right = Put(node.Right, key, value);
+                }
+                else
+                {
+                    node.Value = value;
+                }
+            }
+
+            if (IsRed(node.Right) && !IsRed(node.Left)) node = RotateLeft(node);
+            if (IsRed(node.Left) && IsRed(node.Left.Left)) node = RotateRight(node);
+            if (IsRed(node.Left) && IsRed(node.Right)) FlipColors(node);
+
+            return node;
+        }
+
+        private bool IsRed(Node node)
+        {
+            if (node == null) return false;
+
+            return node.IsRed;
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            var node = _root;
+            while (node != null)
+            {
+                var comp = key.CompareTo(node.Key);
+                if (comp < 0)
+                {
+                    node = node.Left;
+                }
+                else if (comp > 0)
+                {
+                    node = node.Right;
+                }
+                else
+                {
+                    value = node.Value;
+                    return true;
+                }
+            }
+
+            value = default(TValue);
+            return false;
+        }
+
+        public void Remove(TKey key)
+        {
+            //TODO: Implementation here
+        }
+
+        public void Print()
+        {
+            Console.WriteLine();
+            InOrder(_root);
+        }
+        private void InOrder(Node node)
+        {
+            if (node == null) return;
+
+            InOrder(node.Left);
+            Console.Write($"[{node.Key}, {node.Value}] ");
+            InOrder(node.Right);
+        }
+
+        private Node RotateLeft(Node parent)
+        {
+            var rightChild = parent.Right;
+            parent.Right = rightChild.Left;
+            parent.IsRed = true;
+            rightChild.IsRed = parent.IsRed;
+            rightChild.Left = parent;
+
+            return rightChild;
+        }
+
+        private Node RotateRight(Node parent)
+        {
+            var leftChild = parent.Left;
+            parent.Left = leftChild.Right;
+            leftChild.Right = parent;
+            parent.IsRed = true;
+            leftChild.IsRed = parent.IsRed;
+
+            return leftChild;
+        }
+
+        private void FlipColors(Node parent)
+        {
+            parent.IsRed = true;
+            parent.Left.IsRed = false;
+            parent.Right.IsRed = false;
         }
 
         class Node
@@ -21,6 +127,16 @@ namespace DS
             public TKey Key { get; set; }
             public TValue Value { get; set; }
             public bool IsRed { get; set; }
+
+            public Node(TKey key, TValue value)
+            {
+                this.Key = key;
+                this.Value = value;
+                this.IsRed = true;
+            }
+
+            public Node Left { get; set; } = null;
+            public Node Right { get; set; } = null;
         }
     }
 }
