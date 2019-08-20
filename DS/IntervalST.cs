983940351cc2858a9d19d207df281717c57e549e
Orifjon Narkulov <orifjon9@gmail.com>
2019-08-19 22:08:47 -0600

    Add BTree init (#29)

---
+++ DS/IntervalST.cs
@@ -0,0 +1,12 @@
+namespace DS
+{
+    public class IntervalST
+    {
+        class Node
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            
+        }
+    }
+}
