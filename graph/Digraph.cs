570af38a4ec0c75671ae208bec4d99bf1a2e5194
Orifjon Narkulov <orifjon9@gmail.com>
2019-09-01 18:59:02 -0600

    Add Digraph (#35)

---
+++ graph/Digraph.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+
+namespace Graph
+{
+    public class Digraph
+    {
+        private readonly int _vertices;
+        private List<int>[] _edgeAdj;
+        private int _adgesCount = 0;
+
+        public Digraph(int vertices)
+        {
+            _vertices = vertices;
+            _edgeAdj = new List<int>[vertices];
+
+            for (int i = 0; i < vertices; i++)
+            {
+                _edgeAdj[i] = new List<int>();
+            }
+        }
+
+        public void AddEdge(int v, int w)
+        {
+            _edgeAdj[v].Add(w);
+            _adgesCount++;
+        }
+
+        public List<int> Adjacent(int v) => _edgeAdj[v];
+
+        public int AdgesCount() => _adgesCount;
+        public int VerticesCount() => _vertices;
+    }
+}
