ae175881900535ef67cde74f66fc6f5587eb7a03
ORIFJON NARKULOV <orifjon9@gmail.com>
2019-09-01 22:18:09 -0600

    Add Diagram Search

---
+++ graph/DiagramSearch.cs
@@ -0,0 +1,50 @@
+namespace Graph
+{
+    public class DiagramSearch
+    {
+        private int?[] _visitedAdj;
+        private bool[] _marked;
+        public DiagramSearch(Digraph digraph, int startIndex)
+        {
+            var size = digraph.VerticesCount();
+
+            _visitedAdj = new int?[size];
+            _marked = new bool[size];
+
+            DS(digraph, startIndex);
+        }
+
+        private void DS(Digraph digraph, int index)
+        {
+            _marked[index] = true;
+
+            foreach (var item in digraph.Adjacent(index))
+            {
+                if(!_marked[item])
+                {
+                    DS(digraph, item);
+                    _visitedAdj[item] = index;
+                }
+            }
+        }
+
+        public string GetPath(int until)
+        {
+            if(until >= _visitedAdj.Length)
+            {
+                throw new System.Exception("There is not path for this destination");
+            }
+
+            var path = $"->{until}";
+            var index = _visitedAdj[until];
+
+            while(index != null)
+            {
+                path = $"->{index}" + path;
+                index = _visitedAdj[index.Value];
+            }
+
+            return path;
+        }
+    }
+}
